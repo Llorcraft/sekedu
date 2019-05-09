7285ac574ee101a49c259eef3a2684702b45d75f
Luis R. Estrada Llorca <luis.estrada@gruposolutio.com>
2019-05-09 14:56:01 +0200

    user profile

--- src/components/userProfile/userProfile.component.ts
+++ src/components/userProfile/userProfile.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'user-profile',
     templateUrl: './userProfile.component.html',
-    styleUrls: ['./userProfile.component.scss']
+    styleUrls: ['./userProfile.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class UserProfileComponent {
     constructor() { }
